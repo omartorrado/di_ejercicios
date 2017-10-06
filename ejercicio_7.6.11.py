8f4fbe60852dcc9cbb7c78a2b739f47cace02cad
omartorrado <frikimoro@gmail.com>
2017-10-06 11:54:36 +0200

    ejercicios

--- ejercicio_7.6.11.py
+++ ejercicio_7.6.11.py
@@ -1,5 +1,19 @@
 def plegadoText(texto,longitud):
-    #Sin hacer
-    print()
-
+    pos=1
+    posini=0
+    long=15
+    print(len(texto))
+    while (long==15):
+        textoNuevo=texto[posini:longitud+posini]
+        long=len(textoNuevo)
+        print("Texto Inicial: "+textoNuevo+"  ,longitud: "+str(long))
+        if long==15:
+            while textoNuevo[long-pos]!=" ":
+                textoNuevo=texto[posini:(long-pos+posini)]
+                pos+=1
+        print("Texto final: "+textoNuevo)
+        posini=posini+len(textoNuevo)-1
+        print("Posini: "+str(posini))
+        pos=1
+    print(posini+longitud)
 plegadoText("En un lugar de la mancha, de cuyo nombre no quiero acordarme, vivia un hidalgo...",15)
